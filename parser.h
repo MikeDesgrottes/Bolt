a299b57b0128b5e8d14490a0a35d231863292fdf
Mike Desgrottes <MkDesgrottes@gmail.com>
2014-01-24 16:45:52 -0500

    update

--- parser.h
+++ parser.h
@@ -5,10 +5,21 @@
 #include <stdlib.h>
 #include "Person.h"
 
+typedef struct dtb List;
+
+struct dtb{
+    char* value;
+    List *next;
+};
+extern List *LIST;
 int index_of(char *str, char st);
 char* print(char *string, int index);
 char* print_before(char *str, int index);
 Person* read(char* str);
 void write(Person* head);
+List* get_end_list(List *list);
+void write_to_dtb(Person *head);
+List* Create_list();
+void initialize();
 
 #endif
