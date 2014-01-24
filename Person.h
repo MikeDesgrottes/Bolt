3311682bf89145391564a30dad004701cb67a150
Mike Desgrottes <MkDesgrottes@gmail.com>
2014-01-23 19:59:47 -0500

    header file

---
+++ Person.h
@@ -0,0 +1,28 @@
+#ifndef PERSON_H_
+#define PERSON_H_
+
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct fri friends;
+typedef struct Per Person;
+struct Per{
+    char *fname;
+    char *lname;
+    int age;
+    char *description;
+    friends *lfriends;
+};
+struct fri{
+    char* name;
+    friends *next;
+};
+Person* Create(char *fname, char *lname, int age, char *description);
+friends* get_end(Person *p);
+void add_friend(Person *head, Person *friend);
+void list_friends(Person *head);
+int count_friend(Person *head);
+void destroy(void *head);
+void info(Person *head);
+char* get_friends(Person* head);
+#endif
